3880e40953b098939caaef6e421733e748cf223a
Wendell-Z <Wendell-Z>
2020-06-22 19:56:36 +0800

    P70-Climb Stairs

---
+++ src/main/java/sn/P70.java
@@ -0,0 +1,42 @@
+package sn;
+
+/**
+ * Climb Stairs
+ *
+ * @author wendell
+ * @date 2020年6月22日18:56:21
+ */
+public class P70 {
+
+
+    //转置
+    public int climbStairs(int n) {
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        return sum;
+    }
+
+    //递归超时
+    public int recursive_way(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        if (n >= 3) {
+            return recursive_way(n - 1) + recursive_way(n - 2);
+        }
+        return -1;
+    }
+
+    //dp
+
+
+}
